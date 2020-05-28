a76c2acb7c9193f63791924ef2ec9493026b11d2
limaoxiang-hjo <56469999+limaoxiang-hjo@users.noreply.github.com>
2020-05-28 10:59:18 +0800

    Initial commit

---
+++ app/src/main/java/com/liicon/appupdatedemo/utils/MXUpdateTool.java
@@ -0,0 +1,20 @@
+package com.liicon.appupdatedemo.utils;
+
+import android.content.Context;
+/*
+* app更新检测和下载
+* */
+public class MXUpdateTool {
+
+    private Context mContext;
+    /*检查版本号*/
+    private String checkVersionUrl;
+
+    public MXUpdateTool(Context context){
+        this.mContext = context;
+    }
+
+    public void checkVersion(){
+
+    }
+}
